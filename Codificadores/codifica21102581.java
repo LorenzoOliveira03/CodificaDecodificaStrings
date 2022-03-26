5cfaaf45b92a686995980022e9d1bbb16ad58d7d
LorenzoOliveira03 <loriturcato05@gmail.com>
2022-03-26 20:26:31 -0300

    CodificadorFinal

---
+++ Codificadores/codifica21102581.java
@@ -0,0 +1,36 @@
+package Codificadores;
+
+public class codifica21102581 implements Codifica {
+
+    private int multi= 5;
+
+    public String getNomeAutor() {
+        return "Lorenzo T Olivera";
+    }
+
+    public String getMatriculaAutor() {
+        return "21102581";
+    }
+
+    @Override
+    public String codifica(String louco) {
+        String codificada = "";
+        for(int i = 0; i < louco.length(); i++){
+            codificada += Integer.toString(this.multi*((int)louco.charAt(i)));
+            codificada+= " ";
+        }
+        return codificada;
+    }
+
+    @Override
+    public String decodifica(String louco) {
+        String codificada = "";
+        String [] louca = louco.split(" ");
+        for(int i = 0; i < louca.length; i++){
+            codificada  += (char) ((Integer.parseInt(louca[i]))/this.multi);
+        }
+
+        return codificada;
+    }
+    
+}
